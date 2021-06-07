d2c09a7f273b015ce60ae8dcb2d2349370389f59
Devinda <devindarajapaksha12@gmail.com>
2021-06-07 23:04:40 +0530

    change index.dart file add Model.dart

--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -1,64 +1,82 @@
+import 'dart:ui';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_quize_app/theme/colors.dart';
+
+import 'Model.dart';
 
 class IndexPage extends StatefulWidget{
   @override
   _IndexPageState createState() => _IndexPageState();
 }
-class _IndexPageState extends State<IndexPage>{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Listing User"),
-      ),
-      body: getBody(),
-    );
-  }
-  Widget getBody(){
-    List item = [
-      "1"
-      "2"
-    ];
-    return ListView.builder(
-        itemBuilder: (context,index){
-      return getCard();
+class _IndexPageState extends State<IndexPage> {
+
+
+  final fb = FirebaseDatabase.instance.reference().child("Questions");
+  List <Model> list = List();
+
+  void initState() {
+    super.initState();
+    fb.once().then((DataSnapshot snap) {
+      var data = snap.value;
+      list.clear();
+      data.forEach((key, value) {
+        Model model = new Model(
+          Answer: value['Answer'],
+          Option1: value['Option1'],
+          Option2: value['Option2'],
+          Option3: value['Option3'],
+          Option4: value['Option4'],
+          key: key,
+        );
+        print(value['Answer']);
+        print("ss");
+        list.add(model);
+      });
+      setState(() {
+
+      });
     });
   }
-  Widget getCard(){
-    return Card(
-      child:Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ListTile(
-          title: Row(
+
+  @override
+  Widget build(BuildContext context) {
+    Widget UI(String answer, String option1, String key) {
+      return new GestureDetector(
+        onLongPress: () {},
+        onTap: () {},
+        child: Card(
+          child: Column(
             children: <Widget>[
-              Container(
-                width: 60,
-                height: 60,
-                decoration: BoxDecoration(
-                  color: primary,
-                  borderRadius: BorderRadius.circular(60/2),
-                  image :  DecorationImage(
-                      fit :BoxFit.cover,
-                      image:
-                  NetworkImage("https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2ZpbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" )
-                  )
+              Text(
+                answer,
+                style: TextStyle(
+                    color: Colors.red,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
                 ),
-              ),
-              SizedBox(width: 20,),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text("Sopheamen Van", style: TextStyle(fontSize:17),),
-                  SizedBox(width: 20),
-                  Text("Sopheamen Van", style: TextStyle(color: Colors.grey),),
-                ],
               )
             ],
           ),
         ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home page"),
+        backgroundColor: Colors.purple,
+      ),
+      body: new Container(
+        child: list.length == 0? Text("Data is null") : new ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (_, index) {
+              return UI(
+                  list[index].Answer, list[index].Option1, list[index].Option2);
+            }
+        ),
       ),
     );
   }
-
 }
